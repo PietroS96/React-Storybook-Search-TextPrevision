eb37b5b7fcaf9f322d469abc4fc12be58b17b4a3
Pietro Scaduto <pietro.scaduto25@gmail.com>
2022-04-11 12:35:06 +0200

    feat: add searchForm custom hook

--- src/App.js
+++ src/App.js
@@ -1,23 +1,38 @@
-import React, { useState } from "react";
+import React from "react";
 import Autocomplete from "react-autocomplete";
-import { useSearch, useDebounce } from "./hooks";
+import { useSearch, useDebounce, useSearchForm } from "./hooks";
+
+import Input from "./components/Input";
 
 function App() {
-  const [value, setValue] = useState("");
+  const { searchValue, onSearchChange } = useSearchForm();
+  const { articles } = useSearch(useDebounce(searchValue));
 
-  const { articles } = useSearch(useDebounce(value));
   return (
     <Autocomplete
-      getItemValue={(item) => item.label}
       items={articles}
+      renderInput={Input}
+      inputProps={{ placeholder: "Inserisci Testo.." }}
+      getItemValue={(item) => item.label}
+      renderMenu={(children, value, style) => {
+        return articles && articles.length ? (
+          <div style={{ ...style }} className="input-suggestions">
+            {children}
+            <a href={`/search?query=${value}`} className="search-link">
+              Vedi tutti i risultati
+            </a>
+          </div>
+        ) : (
+          <></>
+        );
+      }}
       renderItem={(item, isHighlighted) => (
         <div key={item.id} style={{ background: isHighlighted ? "lightgray" : "white" }}>
           {item.label}
         </div>
       )}
-      value={value}
-      onChange={(e) => setValue(e.target.value)}
-      onSelect={(val) => setValue(val)}
+      value={searchValue}
+      onChange={onSearchChange}
     />
   );
 }
